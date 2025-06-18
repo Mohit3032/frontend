31bd5170f91c613178c96214275ee516b09eac4f
Mohit3032 <138758156+Mohit3032@users.noreply.github.com>
2025-06-18 07:16:15 +0530

    chemfron

--- src/pages/_document.jsx
+++ src/pages/_document.jsx
@@ -4,6 +4,10 @@ export default function Document() {
   return (
     <Html>
       <Head>
+        {/* Favicon */}
+        <link rel="icon" href="/favicon.ico" type="image/png" />
+
+        {/* Fonts */}
         <link
           href="https://fonts.googleapis.com/css2?family=Archivo:wght@400;500;600;700;800&family=Days+One&display=swap"
           rel="stylesheet"
