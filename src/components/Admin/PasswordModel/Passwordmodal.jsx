9bf014826846ac8995bb33ca03842d22aff925b4
Mohit3032 <138758156+Mohit3032@users.noreply.github.com>
2025-06-18 07:07:19 +0530

    chemfron

--- src/components/Admin/PasswordModel/Passwordmodal.jsx
+++ src/components/Admin/PasswordModel/Passwordmodal.jsx
@@ -36,32 +36,33 @@ const Passwordmodal = ({ onLoginSuccess }) => {
 
   return (
     <>
-      <div className={styles["custom-modal-overlay"]}>
-        <div className={styles["custom-modal"]}>
-          <h2>Admin Login</h2>
-          <div className={styles["password-form"]}>
-            <div>
-              <label htmlFor="adminId">Admin ID:</label>
-              <input
-                type="text"
-                id="adminId"
-                value={adminId}
-                onChange={(e) => setAdminId(e.target.value)}
-              />
-            </div>
-            <div>
-              <label htmlFor="password">Password:</label>
-              <input
-                type="password"
-                id="password"
-                value={password}
-                onChange={(e) => setPassword(e.target.value)}
-              />
-            </div>
-            <button onClick={handleLogin}>Login</button>
-          </div>
-        </div>
+     <div className={styles["custom-modal-overlay"]}>
+  <div className={styles["custom-modal"]}>
+    <h2>Admin Login</h2>
+    <form className={styles["password-form"]} onSubmit={handleLogin}>
+      <div>
+        <label htmlFor="adminId">Admin ID:</label>
+        <input
+          type="text"
+          id="adminId"
+          value={adminId}
+          onChange={(e) => setAdminId(e.target.value)}
+        />
       </div>
+      <div>
+        <label htmlFor="password">Password:</label>
+        <input
+          type="password"
+          id="password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+      </div>
+      <button type="submit">Login</button>
+    </form>
+  </div>
+</div>
+
       <ToastContainer />
     </>
   );
